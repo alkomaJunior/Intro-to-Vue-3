b91dc4636b562a2268efdeebab3b5abebafb44e0
Aimé ALOGNIKOU <48490387+alkomaJunior@users.noreply.github.com>
2021-12-18 10:36:51 +0100

    First code challenge
    Add a property description to the data object in main.js and display it in the index.html via a p tag.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const product = 'Socks'
+const description = 'Socks description'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: product,
+            description: description
+        }
+    }
+})
