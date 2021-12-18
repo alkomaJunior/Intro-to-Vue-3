d9ca1d7d1f044da279a86ce1b4e498a334a157dc
Aimé ALOGNIKOU <48490387+alkomaJunior@users.noreply.github.com>
2021-12-18 15:38:23 +0100

    Seventh coding challenge.
    Return computed property based on a bool value.

--- main.js
+++ main.js
@@ -4,21 +4,37 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+            ],
+            selectVariant: 0,
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index){
+            this.selectVariant =  index
+        },
+    },
+    computed: {
+        title() {
+            return this.product + ' ' + this.brand
+        },
+        image(){
+            return this.variants[this.selectVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectVariant].quantity
+        },
+        isOnSale() {
+            if(this.variants[this.selectVariant].quantity != 0) {
+                return this.product + ' ' + this.brand + ' is on sale.'
+            }
+            return this.product + ' ' + this.brand + ' is not on sale.'
         }
     }
 })
