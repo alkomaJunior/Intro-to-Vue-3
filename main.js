03394da4fe921e2db8e94b68fe63f82009113a40
Aimé ALOGNIKOU <48490387+alkomaJunior@users.noreply.github.com>
2021-12-18 12:37:12 +0100

    Fourth code challenge.
    Use v-for to browse a table of sizes.

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [
+                {id: 1, length: '4px'},
+                {id: 2, length: '14px'},
+                {id: 3, length: '24px'},
+                {id: 4, length: '34px'}
+            ]
         }
     }
 })
